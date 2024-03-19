e10dd00254ba025403e1dcbd7fc20c3635f65241
Jorge L. Rosendo <151363153+Rosendo-dev@users.noreply.github.com>
2024-03-19 14:11:19 +0000

    commit dos arquivos base

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home(){
+    return <h1>Minha querida Sheila,
+
+    Cada momento ao seu lado é uma jornada de amor e admiração. Não apenas pela profissional incrível que você é, mas pela pessoa maravilhosa que habita em seu coração. Sua dedicação como fisioterapeuta é inspiradora, assim como seu amor e carinho que transbordam em tudo que faz.
+    
+    Ao ver você cuidar dos outros com tanta ternura e empenho, meu coração se enche de gratidão por tê-la em minha vida. Você é a luz que ilumina meus dias e a força que me impulsiona a ser melhor a cada dia.
+    
+    Nosso amor é como uma terapia para a alma, curando-nos com cada abraço, cada risada compartilhada e cada momento de intimidade. Suas mãos são capazes de curar não apenas os corpos, mas também os corações, e eu me sinto abençoado por receber todo esse amor e cuidado de você.
+    
+    Meu amor por você só cresce a cada dia, como uma flor que desabrocha sob a luz do sol. Você é minha inspiração, minha confidente e minha parceira de vida. Juntos, enfrentaremos qualquer desafio que a vida nos apresentar, sabendo que nosso amor é a força que nos sustenta.
+    
+    Sheila, você é o meu porto seguro, minha razão de sorrir e o amor da minha vida. Obrigado por ser quem você é e por me amar da maneira mais bela e verdadeira.
+    
+    Com todo o meu amor e gratidão,
+    Jorge Lucas</h1>
+}
+
+export default Home;
